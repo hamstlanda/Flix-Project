fa6ac73195b4905d5bed077d9f12f6846e06e185
Liza Liutova <liza@Lizas-MacBook-Air.local>
2018-07-24 21:49:23 -0400

    started x code project

---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Liza Liutova on 7/21/18.
+//  Copyright © 2018 Liza Liutova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
