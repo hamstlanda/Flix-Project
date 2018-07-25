c295f4728554606d402dc8ac957c34ec79d8aa51
Liza Liutova <liza@Lizas-MacBook-Air.local>
2018-07-25 08:25:02 -0400

    added refresh control

--- Flix/Flix/NowPlayingViewController.swift
+++ Flix/Flix/NowPlayingViewController.swift
@@ -14,14 +14,24 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     var movies : [[String: Any]] = []
-    
+    var refreshControl: UIRefreshControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-     
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: . valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         
         tableView.dataSource = self
         tableView.rowHeight = 250
+        fetchMovies()
+    
+        // Do any additional setup after loading the view.
+    }
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchMovies()
+        
+    }
+    func fetchMovies (){
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -32,17 +42,16 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
             }
             if let data = data {
                 let dataDictionary = try!  JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-//                print(dataDictionary)
+                //                print(dataDictionary)
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
-        task.resume()
-        // Do any additional setup after loading the view.
+            task.resume()
+
     }
-    
-   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ The following **required** user stories are complete:
 
 - [x] User sees app icon in home screen and styled launch screen (+1pt)
 - [x] User can scroll through a list of movies currently playing in theaters from The Movie DB API (+5pt)
-- [ ] User can "Pull to refresh" the movie list (+2pt)
+- [x] User can "Pull to refresh" the movie list (+2pt)
 - [ ] User sees a loading state while waiting for the movies to load (+2pt)
 
 The following **stretch** user stories are implemented:
